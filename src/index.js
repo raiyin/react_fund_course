841bad8cd9b578e6a9781774fc34e593a8fc24e3
Maxim P <raiyin@ya.ru>
2022-12-06 16:41:54 +0300

    App conponent. UseState.

--- src/index.js
+++ src/index.js
@@ -4,7 +4,5 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  React.createElement('button', {
-    onClick: ()=>console.log('Click')
-  }, 'Push me!')
+  <App/>,
 );
