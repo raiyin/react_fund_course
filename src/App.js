e3097a01f8610e43153f82dd09ea16a1ea0313cf
Maxim P <raiyin@ya.ru>
2022-12-06 17:03:17 +0300

    Binding. Controlled input.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 
 function App() {
-  const [likes, setLikes] = useState(0) 
+  const [likes, setLikes] = useState(0)
+  const [value, setValue] = useState('Text in the input')
 
-  function increment(){
-    setLikes(likes+1)
+  function increment() {
+    setLikes(likes + 1)
   }
 
-  function decrement(){
-    setLikes(likes-1)
+  function decrement() {
+    setLikes(likes - 1)
   }
 
   return (
     <div className="App">
       <h1>{likes}</h1>
+      <h1>{value}</h1>
+      <input        
+        type="text" 
+        value={value}
+        onChange={event=>setValue(event.target.value)}
+        ></input>
       <button onClick={increment}>Increment</button>
       <button onClick={decrement}>Decrement</button>
     </div>
