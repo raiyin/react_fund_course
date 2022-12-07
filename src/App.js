afaef095dcd147a2544a769edef214bda843ce92
Maxim P <raiyin@ya.ru>
2022-12-07 23:09:31 +0300

    Styles.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,46 @@
 import React, { useState } from 'react';
 import Counter from "./components/Counter";
 import ClassCounter from './components/ClassCounter';
+import './styles/App.css';
 
 function App() {
     const [value, setValue] = useState('Text in the input')
 
   return (
     <div className="App">
-      <Counter/>
-      <ClassCounter/>
+        <div className="post">
+            <div className = "post__content">
+                <strong>1. Javascript</strong>
+                <div>
+                    Javascript - язык программирования.
+                </div>
+            </div>
+            <div className="post_btns">
+                <button>Удалить</button>
+            </div>
+        </div>
+        <div className="post">
+            <div className = "post__content">
+                <strong>1. Javascript</strong>
+                <div>
+                    Javascript - язык программирования.
+                </div>
+            </div>
+            <div className="post_btns">
+                <button>Удалить</button>
+            </div>
+        </div>
+        <div className="post">
+            <div className = "post__content">
+                <strong>1. Javascript</strong>
+                <div>
+                    Javascript - язык программирования.
+                </div>
+            </div>
+            <div className="post_btns">
+                <button>Удалить</button>
+            </div>
+        </div>
     </div>
   )
 }
