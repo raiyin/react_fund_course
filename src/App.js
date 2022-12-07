1050f5ae3db0a980cddf7b6a15ed1cbf1dd0d130
Maxim P <raiyin@ya.ru>
2022-12-07 22:36:14 +0300

    26. Functional component.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,12 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import Counter from "./components/Counter";
 
 function App() {
-  const [likes, setLikes] = useState(0)
-  const [value, setValue] = useState('Text in the input')
-
-  function increment() {
-    setLikes(likes + 1)
-  }
-
-  function decrement() {
-    setLikes(likes - 1)
-  }
+    const [value, setValue] = useState('Text in the input')
 
   return (
     <div className="App">
-      <h1>{likes}</h1>
-      <h1>{value}</h1>
-      <input        
-        type="text" 
-        value={value}
-        onChange={event=>setValue(event.target.value)}
-        ></input>
-      <button onClick={increment}>Increment</button>
-      <button onClick={decrement}>Decrement</button>
+      <Counter/>
     </div>
   )
 }
---
+++ src/components/Counter.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react'
+
+const Counter = function () {
+  const [count, SetCount] = useState(0)
+
+  function increment() {
+    SetCount(count + 1)
+  }
+
+  function decrement() {
+    SetCount(count - 1)
+  }
+
+  return (
+    <div>
+      <h1>{count}</h1>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+    </div>
+  )
+}
+
+export default Counter;
