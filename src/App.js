8b61e4df3b845e799a3380fa2acb53236bd8ce5e
Maxim P <raiyin@ya.ru>
2023-01-06 23:46:02 +0300

    Filtering.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './styles/App.css';
 import PostList from './components/PostList';
 import PostForm from './components/PostForm';
 import MySelect from './components/UI/select/MySelect';
+import MyInput from './components/UI/input/MyInput';
 
 function App() {
 
@@ -13,7 +14,17 @@ function App() {
     ]);
 
     const [selectedSort, setSelectedSort] = useState('');
+    const [searchQuery, setSearchQuery] = useState('');
+    function getSortedPosts() {
+        if (selectedSort) {
+            return [...posts].sort((a, b) => a[selectedSort].localeCompare(b[selectedSort]));
+        }
+        else {
+            return posts;
+        }
+    }
 
+    const sortedPosts = getSortedPosts();
     const createPost = (newPost) => {
         setPosts([...posts, newPost]);
     };
@@ -24,7 +35,6 @@ function App() {
 
     const sortPosts = (sort) => {
         setSelectedSort(sort);
-        setPosts([...posts].sort((a, b) => a[sort].localeCompare(b[sort])));
     };
 
     return (
@@ -32,6 +42,11 @@ function App() {
             <PostForm create={createPost} />
             <hr style={{ margin: '15px 0' }} />
             <div>
+                <MyInput
+                    placeholder='Поиск...'
+                    value={searchQuery}
+                    onChange={e => e.setSearchQuery(e.target.value)}
+                />
                 <MySelect
                     value={selectedSort}
                     onChange={sortPosts}
@@ -43,7 +58,7 @@ function App() {
                 />
             </div>
             {posts.length
-                ? <PostList remove={removePost} posts={posts} title="Список постов 1." />
+                ? <PostList remove={removePost} posts={sortedPosts} title="Список постов 1." />
                 : <h1 style={{ textAlign: 'center' }}> Посты не найдены</h1>
             }
 
