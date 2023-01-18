36d5587250800d08dd945fe2d6a1fd5620c845b8
Maxim P <raiyin@ya.ru>
2023-01-18 22:51:39 +0300

    Animation.

--- src/App.js
+++ src/App.js
@@ -18,7 +18,6 @@ function App() {
     const [modal, setModal] = useState(false);
 
     const sortedPosts = useMemo(() => {
-        console.log("SelectedSort");
         if (filter.sort) {
             return [...posts].sort((a, b) => a[filter.sort].localeCompare(b[filter.sort]));
         }
