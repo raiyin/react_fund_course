c4ddacd0312255a4cef3ae65e6576072e52b5730
Maxim P <raiyin@ya.ru>
2023-01-19 23:11:05 +0300

    Custom hooks. Part1.

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import PostForm from './components/PostForm';
 import PostFilter from './components/UI/PostFilter';
 import MyModal from './components/UI/MyModal/MyModal';
 import MyButton from './components/UI/button/MyButton';
+import usePosts from './hooks/usePosts';
 
 function App() {
 
@@ -16,19 +17,7 @@ function App() {
 
     const [filter, setFilter] = useState({ sort: '', query: '' });
     const [modal, setModal] = useState(false);
-
-    const sortedPosts = useMemo(() => {
-        if (filter.sort) {
-            return [...posts].sort((a, b) => a[filter.sort].localeCompare(b[filter.sort]));
-        }
-        else {
-            return posts;
-        }
-    }, [filter.sort, posts]);
-
-    const sortedAndSearchedPosts = useMemo(() => {
-        return sortedPosts.filter(post => post.title.toLocaleLowerCase().includes(filter.query));
-    }, [filter.query, sortedPosts]);
+    const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
 
     const createPost = (newPost) => {
         setPosts([...posts, newPost]);
