92eecac4ccffd01b992880cc8971c98804b24fae
Maxim P <raiyin@ya.ru>
2023-01-24 21:13:09 +0300

    Right way to solve pagination problem.

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App() {
     const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
     let pageArray = getPageArray(totalPages);
 
-    const [fetchPosts, isPostsLoading, postError] = useFetching(async () => {
+    const [fetchPosts, isPostsLoading, postError] = useFetching(async (limit, page) => {
         const response = await PostService.getAll(limit, page);
         setPosts(response.data);
         const totalCount = response.headers['x-total-count'];
@@ -31,8 +31,8 @@ function App() {
     });
 
     useEffect(() => {
-        fetchPosts();
-    }, [page]);
+        fetchPosts(limit, page);
+    }, []);
 
     const createPost = (newPost) => {
         setPosts([...posts, newPost]);
@@ -45,6 +45,7 @@ function App() {
 
     const changePage = (page) => {
         setPage(page);
+        fetchPosts(limit, page);
     };
 
     return (
