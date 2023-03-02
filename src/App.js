63a4e995c25650b57304bb46e3455e2038375c68
Maxim P <raiyin@ya.ru>
2023-03-03 00:17:38 +0300

    Dynamic navigation. useNavigate.

--- src/App.js
+++ src/App.js
@@ -1,30 +1,14 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
-import About from './pages/About';
-import Posts from './pages/Posts';
+import { BrowserRouter } from 'react-router-dom';
 import Navbar from './components/UI/Navbar/Navbar';
-import Error from './pages/Error';
 import './styles/App.css';
+import AppRouter from './components/AppRouter';
 
 function App() {
     return (
         <BrowserRouter>
             <Navbar />
-            <Routes>
-                <Route path="/about" element={
-                    <About />
-                } />
-                <Route path="/posts" element={
-                    <Posts />
-                } />
-                <Route path="/error" element={
-                    <Error />
-                } />
-                <Route
-                    path="*"
-                    element={<Navigate to="/error" replace />}
-                />
-            </Routes>
+            <AppRouter />
         </BrowserRouter>
     );
 }
--- src/components/PostItem.jsx
+++ src/components/PostItem.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import MyButton from './UI/button/MyButton';
+import { useNavigate, redirect } from "react-router-dom";
 
 const PostItem = (props) => {
+    const navigate = useNavigate();
     return (
         <div className="post">
             <div className="post__content">
@@ -11,7 +13,12 @@ const PostItem = (props) => {
                 <div>{props.post.description}</div>
             </div>
             <div className="post_btns">
-                <MyButton onClick={() => props.remove(props.post)}>Удалить</MyButton>
+                <MyButton onClick={() => navigate(`/posts/${props.post.id}`)}>
+                    Открыть
+                </MyButton>
+                <MyButton onClick={() => props.remove(props.post)}>
+                    Удалить
+                </MyButton>
             </div>
         </div>
     );
